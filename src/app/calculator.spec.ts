bae5ab862c96929e9ffae5b82e1df4361a3ca73b
leo7medina <leo_medina14@hotmail.com>
2022-06-29 00:26:13 -0500

    matchers

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -18,4 +18,37 @@ describe('Test for calculator', () => {
         expect(rta).toEqual(9);
 
     });
+
+
+    it('#multiply should return a for', () => {
+        //Arrange
+        const calculator = new Calculator();
+        //Act
+        const rta = calculator.multiply(1,4);
+        //Assert
+        expect(rta).toEqual(4);
+
+    });
+
+    it('#divide should return a some number', () => {
+        //Arrange
+        const calculator = new Calculator();
+        //Act
+        expect(calculator.divide(6,3)).toEqual(2);
+        expect(calculator.divide(5,2)).toEqual(2.5);
+
+    });
+
+    it('#test matchers', () => {
+        const name = 'Leonardo';
+        let name2;
+
+        expect(name).toBeDefined();
+        expect(name2).toBeUndefined();
+
+        expect(1 + 3 === 3).toBeTruthy();
+        expect(1 + 1 === 3).toBeFalsy();
+
+        expect(5).toBeLessThan(10);
+    });
 });
