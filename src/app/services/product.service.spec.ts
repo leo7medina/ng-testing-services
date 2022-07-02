d5878dd3fa8705332bb1f18044413da533fc2f41
leo7medina <leo_medina14@hotmail.com>
2022-07-02 01:25:59 -0500

    pruebas para POST, PUT, DELETE

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
-import { Product } from '../models/product.model';
+import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
 
 import { ProductService } from './product.service';
 
@@ -27,6 +27,10 @@ fdescribe('ProductService', () => {
     expect(service).toBeTruthy();
   });
 
+  afterEach(()=> {
+    httpController.verify();
+  })
+
   describe('Test for getAllsimple', () => {
     it('should return a product list', (doneFn) => {
       //Arrage
@@ -43,7 +47,6 @@ fdescribe('ProductService', () => {
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpController.expectOne(url);
       req.flush(mokData);
-      httpController.verify();
     });
   });
 
@@ -64,7 +67,6 @@ fdescribe('ProductService', () => {
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpController.expectOne(url);
       req.flush(mokData);
-      httpController.verify();
     });
 
     it('should return a product list with taxes', (doneFn) => {
@@ -90,7 +92,6 @@ fdescribe('ProductService', () => {
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpController.expectOne(url);
       req.flush(mokData);
-      httpController.verify();
     });
 
     it('should send query params with limit 10 and offset 3', (doneFn) => {
@@ -113,7 +114,80 @@ fdescribe('ProductService', () => {
       const params = req.request.params;
       expect(params.get('limit')).toEqual(`${limit}`);
       expect(params.get('offset')).toEqual(`${offset}`);
-      httpController.verify();
     });
   });
+
+  describe('Test for create', () => {
+    it('should return a new product', (doneFn) => {
+      //Arrange
+      const mockData = generateOneProduct();
+      const dto: CreateProductDTO = {
+        title: 'new product',
+        price: 100,
+        images: ['img'],
+        description: 'asdasda',
+        categoryId: 12
+      }
+      //act
+      service.create({...dto}).subscribe( data => {
+        //Assert
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      expect(req.request.body).toEqual(dto);
+      expect(req.request.method).toEqual('POST');
+    });
+  });
+
+  describe('test for update', () => {
+    it('should update a product', (doneFn) => {
+      // Arrange
+      const mockData: Product = generateOneProduct();
+      const dto: UpdateProductDTO = {
+        title: 'new product',
+      }
+      const productId = '1';
+      // Act
+      service.update(productId, {...dto})
+      .subscribe((data) => {
+        // Assert
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      // http config
+      const url = `${environment.API_URL}/api/v1/products/${productId}`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual(dto);
+      req.flush(mockData);
+    });
+  });
+
+  describe('test for delete', () => {
+    it('should delete a product', (doneFn) => {
+      // Arrange
+      const mockData = true;
+      const productId = '1';
+      // Act
+      service.delete(productId)
+      .subscribe((data) => {
+        // Assert
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      // http config
+      const url = `${environment.API_URL}/api/v1/products/${productId}`;
+      const req = httpController.expectOne(url);
+      expect(req.request.method).toEqual('DELETE');
+      req.flush(mockData);
+    });
+  });
+
+
 });
